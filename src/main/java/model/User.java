31e48cfa186cfac9103dd653a34343f4bdf09e4b
sindhu-harish <sindhu.k@edstem.com>
2023-07-06 10:47:31 +0530

    Entity class added

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,23 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+    private String firstName;
+    private String lastName;
+
 }
